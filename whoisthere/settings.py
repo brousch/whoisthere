0d148421af5567bcfa32655ffa3d763026500edf
Ben Rousch <brousch@gmail.com>
2013-06-24 09:49:19 -0400

    Added static assets

--- whoisthere/settings.py
+++ whoisthere/settings.py
@@ -59,7 +59,7 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'dev', 'static')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
-STATIC_URL = '/static/'
+STATIC_URL = 'http://static.whoisthere.orthicomp.com/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
